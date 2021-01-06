ddff843bc97a6b24a3cb51aa9ca67abc8780b20f
karol <karol.jarosinski91@gmail.com>
2021-01-06 06:45:18 +0100

    Gotowe cwiczenie

---
+++ src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public static void main(String[] args) {
+        String brand = "Honda";
+        String model = "Civic";
+        int year = 2009;
+        double price = 90000.5;
+
+        System.out.println("Wybrany samochód to: " + brand + ", model: " + model + ", rocznik: " + year + " w cenie: " + price);
+
+    }
+}
